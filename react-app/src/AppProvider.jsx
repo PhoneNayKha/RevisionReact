6b9c4d85d45004cc975a017b80b65327849f6bfc
Phone Nay Kha <phonenaykha@Phones-MacBook-Air.local>
2024-12-14 22:20:55 +0630

    postman err commit

--- react-app/src/AppProvider.jsx
+++ react-app/src/AppProvider.jsx
@@ -1,45 +1,48 @@
 import { createContext, useContext, useState, useMemo } from "react";
-
 import { createTheme, ThemeProvider, CssBaseline } from "@mui/material";
-
+import { QueryClientProvider, QueryClient } from "react-query";
 import AppRouter from "./AppRouter";
 
 const AppContext = createContext();
+const queryClient = new QueryClient();
 
 export function useApp() {
-	return useContext(AppContext);
+    return useContext(AppContext);
 }
 
 export default function AppProvider() {
-	const [showForm, setShowForm] = useState(false);
-	const [showDrawer, setShowDrawer] = useState(false);
-	const [mode, setMode] = useState("dark");
-	const [auth, setAuth] = useState(false);
+    const [showForm, setShowForm] = useState(false);
+    const [showDrawer, setShowDrawer] = useState(false);
+    const [mode, setMode] = useState("dark");
+    const [auth, setAuth] = useState(false);
 
-	const theme = useMemo(() => {
-		return createTheme({
-			palette: {
-				mode,
-			}
-		});
-	}, [mode]);
+    const theme = useMemo(() => {
+        return createTheme({
+            palette: {
+                mode,
+            },
+        });
+    }, [mode]);
 
-	return (
-		<AppContext.Provider
-			value={{
-				showDrawer,
-				setShowDrawer,
-				showForm,
-				setShowForm,
-				mode,
-				setMode,
+    return (
+        <AppContext.Provider
+            value={{
+                showDrawer,
+                setShowDrawer,
+                showForm,
+                setShowForm,
+                mode,
+                setMode,
                 auth,
                 setAuth,
-			}}>
-			<ThemeProvider theme={theme}>
-				<AppRouter />
-				<CssBaseline />
-			</ThemeProvider>
-		</AppContext.Provider>
-	);
+            }}
+        >
+            <QueryClientProvider client={queryClient}>
+                <ThemeProvider theme={theme}>
+                    <AppRouter />
+                    <CssBaseline />
+                </ThemeProvider>
+            </QueryClientProvider>
+        </AppContext.Provider>
+    );
 }
--- react-app/src/components/Form.jsx
+++ react-app/src/components/Form.jsx
@@ -1,40 +1,61 @@
 import { useRef } from "react";
 
-import {
-    OutlinedInput,
-    IconButton,
-} from "@mui/material";
+import { OutlinedInput, IconButton } from "@mui/material";
 
-import {
-    Add as AddIcon,
-} from "@mui/icons-material";
+import { Add as AddIcon } from "@mui/icons-material";
 
 import { useApp } from "../AppProvider";
 
-export default function Form({ add }) {
+import { useMutation, useQueryClient } from "react-query";
+
+async function postPost(content) {
+    const api = "http://localhost:8080/posts";
+    const res = await fetch(api, {
+        method: "POST",
+        body: JSON.stringify({ content }),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+
+    return res.json();
+}
+
+export default function Form() {
     const inputRef = useRef();
+    const queryClient = useQueryClient();
+
+    const add = useMutation(postPost, {
+        onSuccess: async (item) => {
+            await queryClient.cancelQueries();
+            await queryClient.setQueryData("posts", (old) => {
+                return [item, ...old];
+            });
+        },
+    });
 
     return (
-		<form
-			style={{ marginBottom: 20, display: "flex" }}
-			onSubmit={e => {
-				e.preventDefault();
+        <form
+            style={{ marginBottom: 20, display: "flex" }}
+            onSubmit={(e) => {
+                e.preventDefault();
 
-				const content = inputRef.current.value;
-				content && add(content);
+                const content = inputRef.current.value;
+                content && add.mutate(content);
 
-				e.currentTarget.reset();
-			}}>
-			<OutlinedInput
-				type="text"
-				style={{ flexGrow: 1 }}
-				inputRef={inputRef}
+                e.currentTarget.reset();
+            }}
+        >
+            <OutlinedInput
+                type="text"
+                style={{ flexGrow: 1 }}
+                inputRef={inputRef}
                 endAdornment={
                     <IconButton type="submit">
                         <AddIcon />
                     </IconButton>
                 }
-			/>
-		</form>
-	);
+            />
+        </form>
+    );
 }
